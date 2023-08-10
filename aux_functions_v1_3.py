b276a7c432c2da85093356477baf914725f48aeb
Gamu <guzmyns@hotmail.es>
2023-08-10 17:21:36 +0200

    Added examples for tests in examples_test.py. The bounds of the functions were modified, added Moves tests and it works.

--- aux_functions_v1_3.py
+++ aux_functions_v1_3.py
@@ -20,14 +20,14 @@ def Valid_Labyrinth_Symbols(labyrinth): #Checks if the matrix only has "." and "
 ### All Valid_Move check if the rod can perform that move (Right, left, up, down or rotation) ###  
 ### It is necessary to differentiate if the rod is horizontal or vertical to check the walls and obstacles. ###
 def Valid_Right(x,y,orientation,labyrinth):
-    if (orientation == "vertical" and len(labyrinth[0])>y+1):
+    if (orientation == "vertical" and len(labyrinth[0])>y+1 and x>0 and x+1<len(labyrinth)): #x>0 is necessary, our rod can't walk through walls (analogous for the rest moves).
         return labyrinth[x-1][y+1] == labyrinth[x][y+1] == labyrinth[x+1][y+1] == "." 
     if (orientation == "horizontal" and len(labyrinth[0])>y+2):
         return labyrinth[x][y+2] == "." 
     return False
 
 def Valid_Left(x,y,orientation,labyrinth): 
-    if (orientation == "vertical" and 0<y):
+    if (orientation == "vertical" and 0<y and x>0 and x+1<len(labyrinth)):
         return labyrinth[x-1][y-1] == labyrinth[x][y-1] == labyrinth[x+1][y-1] == "." 
     if (orientation == "horizontal" and 0<y-1):
         return labyrinth[x][y-2] == "." 
@@ -36,15 +36,14 @@ def Valid_Left(x,y,orientation,labyrinth):
 def Valid_Up(x,y,orientation,labyrinth): 
     if (orientation == "vertical" and 0<x-1):
         return labyrinth[x-2][y] == "." 
-    if (orientation == "horizontal" and 0<x):
+    if (orientation == "horizontal" and 0<x and y>0 and y+1<len(labyrinth[0])):
         return labyrinth[x-1][y-1] == labyrinth[x-1][y] == labyrinth[x-1][y+1] == "." 
     return False
 
 def Valid_Down(x,y,orientation,labyrinth): 
-    print(len(labyrinth),x+2)
     if (orientation == "vertical" and len(labyrinth)>x+2):
         return labyrinth[x+2][y] == "." 
-    if (orientation == "horizontal" and len(labyrinth)>x+1):
+    if (orientation == "horizontal" and len(labyrinth)>x+1 and y>0 and y+1<len(labyrinth[0])):
         return labyrinth[x+1][y-1] == labyrinth[x+1][y] == labyrinth[x+1][y+1] == "." 
     return False
 
---
+++ test_v1_0.py
@@ -0,0 +1,152 @@
+#### All test functions ####
+
+from aux_functions_v1_2 import *
+from examples_test import *
+#### Examples we are going to use to Valid_Labyrinth ####
+
+labyrinth = [
+    [".",".",".",".",".",".",".","."], 
+    [".","#",".",".",".",".","#","."],
+    [".","#",".",".",".",".",".","."],
+    [".",".",".",".",".",".",".","."],
+    [".",".",".",".",".",".",".","."],
+    [".","#",".",".",".",".",".","."],
+    [".","#",".",".",".","#",".","."]
+    ]
+
+
+
+def test_Valid_Labyrinth_Range ():
+    assert(Valid_Labyrinth_Range(labyrinth)== True )
+    assert(Valid_Labyrinth_Range(labyrinth_Valid_Range1) == False)
+    assert(Valid_Labyrinth_Range(labyrinth_Valid_Range2) == False)
+
+def test_Valid_Labyrinth_Symbols ():
+    assert(Valid_Labyrinth_Symbols(labyrinth)== True )
+    assert(Valid_Labyrinth_Symbols(labyrinth_Valid_Symbols1) == False)
+    assert(Valid_Labyrinth_Symbols(labyrinth_Valid_Symbols2) == False)
+
+def test_Valid_Right():
+    for x in list(range(0,len(labyrinth_Valid_Moves1))):
+        for y in list(range(0,len(labyrinth_Valid_Moves1[0])-2)):
+            try:
+                assert(Valid_Right(x,y,"horizontal",labyrinth_Valid_Moves1))   ## First we see in a labyrinth with full ".", so we can always move the rod without hit the wall
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the right from ({},{}). ".format(x,y)))
+            
+    for x in list(range(1,len(labyrinth_Valid_Moves1)-1)):
+        for y in list(range(0,len(labyrinth_Valid_Moves1[0])-1)):
+            try:
+                assert(Valid_Right(x,y,"vertical",labyrinth_Valid_Moves1))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the right from ({},{}). ".format(x,y)))
+    for x in list(range(0,len(labyrinth_Valid_Moves2))):                        ## Next, we  see in a labyrinth with full "#", so we can't move the rod
+        for y in list(range(0,len(labyrinth_Valid_Moves2[0]))):
+            try:
+                assert(not Valid_Right(x,y,"horizontal",labyrinth_Valid_Moves2))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the right when it mustn't move from ({},{}). ".format(x,y)))
+    for x in list(range(0,len(labyrinth_Valid_Moves2))):                        
+        for y in list(range(0,len(labyrinth_Valid_Moves2[0]))):
+            try:
+                assert(not Valid_Right(x,y,"vertical",labyrinth_Valid_Moves2))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the right when it mustn't move ({},{}). ".format(x,y)))
+            
+### The following test_Valid_Moves are analogous
+
+def test_Valid_Left():
+    for x in list(range(0,len(labyrinth_Valid_Moves1))):
+        for y in list(range(2,len(labyrinth_Valid_Moves1[0]))):
+            try:
+                assert(Valid_Left(x,y,"horizontal",labyrinth_Valid_Moves1))   
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the left from ({},{}). ".format(x,y)))
+            
+    for x in list(range(1,len(labyrinth_Valid_Moves1)-1)):
+        for y in list(range(1,len(labyrinth_Valid_Moves1[0]))):
+            try:
+                assert(Valid_Left(x,y,"vertical",labyrinth_Valid_Moves1))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the left from ({},{}). ".format(x,y))) 
+
+    for x in list(range(0,len(labyrinth_Valid_Moves2))):                     
+        for y in list(range(0,len(labyrinth_Valid_Moves2[0]))):
+            try:
+                assert(not Valid_Left(x,y,"horizontal",labyrinth_Valid_Moves2))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the left when it mustn't move from ({},{}). ".format(x,y))) 
+    for x in list(range(0,len(labyrinth_Valid_Moves2))):                       
+        for y in list(range(0,len(labyrinth_Valid_Moves2[0]))):
+            try:
+                assert(not Valid_Left(x,y,"vertical",labyrinth_Valid_Moves2))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the left when it mustn't move ({},{}). ".format(x,y)))
+
+
+def test_Valid_Up():
+    for x in list(range(1,len(labyrinth_Valid_Moves1))):
+        for y in list(range(1,len(labyrinth_Valid_Moves1[0])-1)):
+            try:
+                assert(Valid_Up(x,y,"horizontal",labyrinth_Valid_Moves1))   
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the up from ({},{}). ".format(x,y)))
+            
+    for x in list(range(2,len(labyrinth_Valid_Moves1)-1)):
+        for y in list(range(0,len(labyrinth_Valid_Moves1[0]))):
+            try:
+                assert(Valid_Up(x,y,"vertical",labyrinth_Valid_Moves1))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the up from ({},{}). ".format(x,y))) 
+
+    for x in list(range(0,len(labyrinth_Valid_Moves2))):                     
+        for y in list(range(0,len(labyrinth_Valid_Moves2[0]))):
+            try:
+                assert(not Valid_Up(x,y,"horizontal",labyrinth_Valid_Moves2))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the up when it mustn't move from ({},{}). ".format(x,y))) 
+    for x in list(range(0,len(labyrinth_Valid_Moves2))):                       
+        for y in list(range(0,len(labyrinth_Valid_Moves2[0]))):
+            try:
+                assert(not Valid_Up(x,y,"vertical",labyrinth_Valid_Moves2))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the up when it mustn't move ({},{}). ".format(x,y)))
+            
+def test_Valid_Down():
+    for x in list(range(0,len(labyrinth_Valid_Moves1)-1)):
+        for y in list(range(1,len(labyrinth_Valid_Moves1[0])-1)):
+            try:
+                assert(Valid_Down(x,y,"horizontal",labyrinth_Valid_Moves1))   
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the down from ({},{}). ".format(x,y)))
+            
+    for x in list(range(1,len(labyrinth_Valid_Moves1)-2)):
+        for y in list(range(0,len(labyrinth_Valid_Moves1[0]))):
+            try:
+                assert(Valid_Down(x,y,"vertical",labyrinth_Valid_Moves1))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the down from ({},{}). ".format(x,y))) 
+
+    for x in list(range(0,len(labyrinth_Valid_Moves2))):                     
+        for y in list(range(0,len(labyrinth_Valid_Moves2[0]))):
+            try:
+                assert(not Valid_Down(x,y,"horizontal",labyrinth_Valid_Moves2))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the down when it mustn't move from ({},{}). ".format(x,y))) 
+    for x in list(range(0,len(labyrinth_Valid_Moves2))):                       
+        for y in list(range(0,len(labyrinth_Valid_Moves2[0]))):
+            try:
+                assert(not Valid_Down(x,y,"vertical",labyrinth_Valid_Moves2))
+            except AssertionError :
+                raise( AssertionError("Error trying to move the rod to the down when it mustn't move ({},{}). ".format(x,y)))
+
+
+### It is not necessary a test for Valid_Rotation, it just depends on the other Valid_Moves
+
+test_Valid_Labyrinth_Range()
+test_Valid_Labyrinth_Symbols()
+test_Valid_Right()
+test_Valid_Left()
+test_Valid_Up()
+test_Valid_Down()
+
